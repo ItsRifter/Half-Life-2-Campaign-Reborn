7a0bbae35475281e2cf1d2c26f2ecc87a419db83
ItsRifter <d_thomas_smith30@hotmail.com>
2022-06-29 16:00:05 +0100

    Full reset

--- gamemode/shared.lua
+++
@@ -1,9 +0,0 @@
-AddCSLuaFile()
-
-GM.Name = "Half-Life 2: Campaign - Reborn"
-GM.Author = "Rifter"
-GM.Email = "d_thomas_smith30@hotmail.com"
-GM.Website = "N/A"
-
-GM.ServerName = "Half-Life 2: Campaign - Reborn Official Server"
-GM.Version = "V0.07"
